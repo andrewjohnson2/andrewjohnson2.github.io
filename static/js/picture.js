51c3337bf9df873427b8def56ea8cdbeae6ae7e3
Andrew R Johnson <realaj@umich.edu>
2019-11-23 18:18:15 -0500

    small update

--- static/js/picture.js
+++ static/js/picture.js
@@ -1,80 +1,122 @@
 $(function() {
   $('#1').click(function() {
     if ($('#2').is(':visible')) {
-      $('#2').hide();
-      $('#3').hide();
-      $('#4').show();
-      $('#5').show();
-      $('#6').show();
+      adjust('#1', true);
     } else {
-      $('#2').show();
-      $('#3').show();
+      adjust('#1', false);
     }
   });
 
   $('#2').click(function() {
     if ($('#1').is(':visible')) {
-      $('#1').hide();
-      $('#3').hide();
-      $('#4').show();
-      $('#5').show();
-      $('#6').show();
+      adjust('#2', true);
     } else {
-      $('#1').show();
-      $('#3').show();
+      adjust('#2', false);
     }
   });
 
   $('#3').click(function() {
     if ($('#1').is(':visible')) {
-      $('#1').hide();
-      $('#2').hide();
-      $('#4').show();
-      $('#5').show();
-      $('#6').show();
+      adjust('#3', true);
     } else {
-      $('#1').show();
-      $('#2').show();
+      adjust('#3', false);
     }
   });
 
   $('#4').click(function() {
-    if ($('#5').is(':visible')) {
-      $('#5').hide();
-      $('#6').hide();
-      $('#1').show();
-      $('#2').show();
-      $('#3').show();
+    if ($('#1').is(':visible')) {
+      adjust('#4', true);
     } else {
-      $('#5').show();
-      $('#6').show();
+      adjust('#4', false);
     }
   });
 
   $('#5').click(function() {
-    if ($('#4').is(':visible')) {
-      $('#4').hide();
-      $('#6').hide();
-      $('#1').show();
-      $('#2').show();
-      $('#3').show();
+    if ($('#1').is(':visible')) {
+      adjust('#5', true);
     } else {
-      $('#4').show();
-      $('#6').show();
+      adjust('#5', false);
     }
   });
 
   $('#6').click(function() {
-    if ($('#4').is(':visible')) {
-      $('#4').hide();
-      $('#5').hide();
-      $('#1').show();
-      $('#2').show();
-      $('#3').show();
+    if ($('#1').is(':visible')) {
+      adjust('#6', true);
     } else {
-      $('#4').show();
-      $('#5').show();
+      adjust('#6', false);
     }
   });
+
+  $('#7').click(function () {
+    if ($('#1').is(':visible')) {
+      adjust('#7', true);
+    } else {
+      adjust('#7', false);
+    }
+  });
+
+  $('#8').click(function () {
+    if ($('#1').is(':visible')) {
+      adjust('#8', true);
+    } else {
+      adjust('#8', false);
+    }
+  });
+
+  $('#9').click(function () {
+    if ($('#1').is(':visible')) {
+      adjust('#9', true);
+    } else {
+      adjust('#9', false);
+    }
+  });
+
+  function adjust(id_tag, enlargen) {
+    console.log(id_tag);
+    let NUM_TAGS = 10;
+    let i = 0;
+    let tag = id_tag.substring(id_tag.search('#') + 1);
+    console.log(tag);
+    console.log(enlargen)
+
+    if (enlargen) {
+      if (tag <= 3) {
+        $("#r2").hide();
+        $("#r3").hide();
+        $("#r1").show();
+      } else if (tag <= 6) {
+        $("#r2").show();
+        $("#r3").hide();
+        $("#r1").hide();
+      } else {
+        $("#r2").hide();
+        $("#r3").show();
+        $("#r1").hide();
+      }
+    } else {
+      $("#r1").show();
+      $("#r2").show();
+      $("#r3").show();
+    }
+
+    for (; i < tag; ++i) {
+      if (enlargen) {
+        $('#' + i).hide();
+      } else {
+        console.log('#' + i)
+        $('#' + i).show();
+      }
+      
+    }
+    i += 1
+    for (; i < NUM_TAGS; ++i) {
+      if (enlargen) {
+        $('#' + i).hide();
+      } else {
+        $('#' + i).show();
+      }
+    }
+  }
+
 
 })
